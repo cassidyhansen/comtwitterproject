ff27633aa30dd0fd17e367c091e790809c720ca3
cassidyhansen <63433614+cassidyhansen@users.noreply.github.com>
2020-04-10 13:06:28 -0400

    Update twitterapiproject.py

--- twitterapiproject.py
+++ twitterapiproject.py
@@ -30,3 +30,29 @@ except BaseException as e:
 
 with open('byu.json', 'w') as f:
     json.dump(result,f)
+    
+#converting data to a Pandas friendly format
+import pandas as pd
+byu_df = pd.read_json (r'C:\Users\Chase\Desktop\Coding\twitter-cdsw-master\twitter-cdsw-master\byu.json')
+export_csv = df.to_csv (r'C:\Users\Chase\Desktop\Coding\twitter-cdsw-master\twitter-cdsw-master\byu.csv', index = None, header=True)
+
+#reading file and getting ready to make a graph
+import matplotlib.pyplot as plt
+import pandas as pd
+df = pd.read_csv(r'C:\Users\Chase\Desktop\Coding\twitter-cdsw-master\twitter-cdsw-master\byu.csv')
+
+#graphing
+df.index = pd.to_datetime(df.created_at) #converting time to analysis friendly format
+plt.figure(figsize=(16,5))
+plt.xlabel('created_at')
+
+ax1 = df.favorite_count.plot(color='blue', grid=True, label='Favorites')
+ax2 = df.retweet_count.plot(color='green', grid=True, secondary_y=True, label='Retweets')
+
+ax1.legend(loc=1)
+ax2.legend(loc=2)
+
+plt.show()
+
+#did I repurpose this code? ABSOLUTELY. Shout out to Josh Blum
+
